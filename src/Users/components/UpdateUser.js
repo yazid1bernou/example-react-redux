b9e6c5529646e099829e20e54caab410add6a4f4
unknown <yazid1bernou@gmail.com>
2023-12-17 09:49:58 +0000

    update user with redux

--- src/Users/components/UpdateUser.js
+++ src/Users/components/UpdateUser.js
@@ -1,11 +1,43 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {UpdateUserAction} from "../actions";
+import { useNavigate } from "react-router-dom";
 
 const UpdateUser = () => {
     const {id} = useParams();
+    const user =  useSelector(data => data.users.find((u) => u.id === parseInt(id)));
+    const [name , setName] = useState(user.name); 
+    const [email , setEmail] = useState(user.email); 
+    const dispatch =  useDispatch();
+    const navigate =  useNavigate();
+    
+
+    const handleClick = () => {
+        dispatch(UpdateUserAction({
+            id :id ,
+            name : name ,
+            email : email
+        }))
+        navigate('/')
+    }
     return (
-        <div>
-              UpdateUser :  {id}
-        </div>
+           <div>
+                <form>
+                <div>
+                    <label> Name</label>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div>
+                    <button onClick={handleClick}>Submit</button>
+                </div>
+
+                </form>
+           </div>
     )
 }
 
