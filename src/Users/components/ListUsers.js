0598c975253f2d80edc98921121b24bf2d07f170
unknown <yazid1bernou@gmail.com>
2023-12-17 10:02:12 +0000

    delete user with redux

--- src/Users/components/ListUsers.js
+++ src/Users/components/ListUsers.js
@@ -1,9 +1,15 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import {DeleteUserAction} from "../actions"; 
 function  ListUsers () {
-    
+     
     const users = useSelector((data)=>data.users);
+    const dispatch = useDispatch();
+
+    const handleDelete = (id) => {
+       dispatch(DeleteUserAction(id))
+    }
     return (
         <div>
               <div>
@@ -29,7 +35,7 @@ function  ListUsers () {
                                     <td>{user.email}</td>
                                     <td>
                                         <Link to={`/update-user/${user.id}`}><button>Edit</button> </Link>
-                                        <button>Delete</button>
+                                        <button onClick={() => handleDelete(user.id)}>Delete</button>
                                     </td>
                                 </tr>
                               )
